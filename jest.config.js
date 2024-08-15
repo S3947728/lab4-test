1c9ad270d2424d89f26b088264b3736dc14185d9
S3947728 <S3947728@student.rmit.edu.au>
2024-08-15 13:10:39 +1000

    Fixed Minor Lint

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,7 @@
 module.exports = {
-    reporters: 
-    [
+        reporters: [
         'default',
-        [
-            'jest-junit',
-            {
-                outputDirectory: 'reports',
-                outputName: 'report.xml'
-            }]
-    ]
-};
+        ['jest-junit', { outputDirectory: 'reports',
+        outputName: 'report.xml' }],
+        ],
+    };
